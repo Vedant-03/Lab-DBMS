17bb70188f3b6695978fcc24aabcb7cacccf75b0
Vxrtesh <varteshbarwar27@gmail.com>
2022-11-10 15:28:02 +0530

    final

--- item.js
+++ item.js
@@ -33,5 +33,45 @@ router.post('/', (req,res) => {
     });
 });
 
+router.post('/upd', (req,res) => {
+    var lb = req.body.lab_u;
+    var room = req.body.room_no_u;
+    var itid = req.body.item_id;
+    var image = req.body.imgs_u;
+    var mdl = req.body.model_u;
+    var quantity = req.body.qty_u;
+    var about = req.body.desc_u;
+
+    var lbb = lb.toLowerCase();
+    lbb = lbb + "_" + room;
+    console.log(lbb);
+    
+    if(itid && lb && room){
+        if(quantity <= 0){
+            query = `DELETE FROM ${lbb} WHERE i_id = ${itid}`;
+            database.query(query, (error, data) => {});
+        }
+        else{
+            if(image){
+                query = `UPDATE ${lbb} SET image = "${image}" WHERE i_id = ${itid}`;
+                database.query(query, (error, data) => {});
+            }
+            if(mdl){
+                query = `UPDATE ${lbb} SET model = "${mdl}" WHERE i_id = ${itid}`;
+                database.query(query, (error, data) => {});
+            }
+            if(quantity){
+                query = `UPDATE ${lbb} SET quantity = "${quantity}" WHERE i_id = ${itid}`;
+                database.query(query, (error, data) => {});
+            }
+            if(about){
+                query = `UPDATE ${lbb} SET descr = "${about}" WHERE i_id = ${itid}`;
+                database.query(query, (error, data) => {});
+            }
+        }
+        res.redirect("/");
+        res.end();
+    }
+});
 
 module.exports = router;
--- upd_q.js
+++ upd_q.js
@@ -14,7 +14,9 @@ router.get("/", (req,res) => {
 
 router.post('/', (req,res) => {
     var qid = req.body.qid;
-    query = `UPDATE query SET solved = 1 WHERE q_id = ${qid}`;
+    var indate = new Date();
+    var date = indate.getFullYear() + "-" + (indate.getMonth()+1) + "-" + indate.getDate();
+    query = `UPDATE query SET solved = 1, solve_date = "${date}"  WHERE q_id = ${qid}`;
     database.query(query, (error,data) => {
         res.redirect("/");
         res.end();
